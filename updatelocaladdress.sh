8110db0855d3ea6240be9bc77eef8a229d0c7200
Ryan Borstelmann <ryan@ryanb.tv>
2019-04-18 15:42:53 -0500

    added prereq's

--- updatelocaladdress.sh
+++ updatelocaladdress.sh
@@ -1,5 +1,12 @@
 #!/bin/vbash
 
+############
+# Pre-Requisites:
+# 1) File at /config/scripts/previous.ip with the WAN IP "previously". This will be compared to the new Dynamic DNS IP.
+# 2) "postscript" option for ddclient added to EdgeOS configuration. For example: 'set service dns dynamic interface eth0 service dyndns options postscript=/config/scripts/updatelocaladdress.sh"
+############
+
+
 #Get hostname
 HOSTNAME=$(hostname)
 currentIp=$1
